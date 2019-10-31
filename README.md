34dfd0863b480b53ad0f31df2713968b918a2adb
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2019-10-30 23:08:29 -0400

    Initial simple application which demonstrates how to draw in WPF.

---
+++ README.md
@@ -0,0 +1,5 @@
+Example of how to draw in WPF using GDI+
+([`System.Drawing`](https://docs.microsoft.com/dotnet/api/system.drawing?view=netframework-4.8))-style
+[`Bitmap`](https://docs.microsoft.com/dotnet/api/system.drawing.bitmap?view=netframework-4.8)
+and
+[`Grapics`](https://docs.microsoft.com/dotnet/api/system.drawing.graphics?view=netframework-4.8).
---
+++ WpfAppDrawBitmap/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfAppDrawBitmap
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using var bitmap = new Bitmap((int)Width, (int)Height);
+            using var g = Graphics.FromImage(bitmap);
+            g.Clear(Color.Red);
+            g.FillRectangle(Brushes.Green, 0, 0, (float)Width / 2, (float)Height / 2);
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            using var pngStream = new MemoryStream();
+            bitmap.Save(pngStream, ImageFormat.Png);
+            pngStream.Position = 0;
+            bitmapImage.StreamSource = pngStream;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            image.Source = bitmapImage;
+        }
+    }
+}
